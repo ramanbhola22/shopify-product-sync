c106576e191a3216b5c0729f4cc08e4792b098da
Arsh Kalsi <arsh.seraphic@gmail.com>
2023-06-14 17:02:46 +0530

    fix build file

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ npm install
 
 cd frontend
 npm install
-export SHOPIFY_API_KEY=3f731947689e7036a17186bff1bea0ab && npm run build
+export SHOPIFY_API_KEY=898a0c499ba55227431ae1c6a16d2ce8 && npm run build
